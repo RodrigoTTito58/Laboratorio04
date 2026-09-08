326937f3cd601a8143a89b7f0ca16dc2a500d099
RodrigoTTito58 <rodrigottito32@gmail.com>
2026-09-08 10:16:55 -0500

    Cambio en rama de conflicto

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.lab04
-//Hola Soy Rodrigo
+//Hola modificacion dle compañero
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
