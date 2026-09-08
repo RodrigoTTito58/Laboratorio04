7eeb9aab60b355b3ddcc86c9d927735540257e4f
RodrigoTTito58 <rodrigottito32@gmail.com>
2026-09-08 09:44:34 -0500

    Tercer commit añadido de Button Rodrigo Ttito

---
+++ app/src/main/java/com/example/lab04/FloatingActionButton.kt
@@ -0,0 +1,12 @@
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MiBotonFlotanteComponente() {
+    FloatingActionButton(
+        onClick = { /* Acción al hacer clic */ }
+    ) {
+        Text(text = "+")
+    }
+}
